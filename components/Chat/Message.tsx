57255a8ad89a015f587535febd42c3fb772a6d70
lvd536 <nbikiti@gmail.com>
2025-12-06 15:33:26 +0400

    - Message styling changes

--- components/Chat/Message.tsx
+++ components/Chat/Message.tsx
@@ -9,14 +9,14 @@ interface IProps {
 
 export default function Message({ text, time, isUser }: IProps) {
     const messageStyle = isUser
-        ? "self-end bg-user-message-bg rounded-xl p-2 min-w-25 max-w-4/6"
-        : "bg-message-bg rounded-xl p-2 min-w-25 max-w-4/6";
+        ? "self-end bg-user-message-bg rounded-xl p-2 w-fit min-w-25 max-w-4/6"
+        : "bg-message-bg rounded-xl p-2 min-w-25 w-fit max-w-4/6";
     const timeStyle = isUser
         ? "text-white/90 text-xs"
         : "text-[#707991] text-xs";
     return (
         <li className={messageStyle}>
-            <p>{text}</p>
+            <p className="max-w-100 text-justify">{text}</p>
             <div className="flex justify-end gap-2">
                 <p className={timeStyle}>{time}</p>
                 <Image alt="" src={mark}></Image>
