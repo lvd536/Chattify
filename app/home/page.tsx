95b365ba3d628f7aac0704ec27b4134d085c39ca
lvd536 <nbikiti@gmail.com>
2025-12-07 14:10:46 +0400

    - Add select chat message

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,11 @@
-import NavBar from "@/components/NavBar/NavBar";
 export default async function page() {
     return (
         <div className="w-8/12">
-            {/* <ChatHead avatarUrl="" lastSeen="19:26" name="David Tumberg" /> */}
-            {/* <ChatBody /> */}
+            <div className="flex h-full w-full items-center justify-center">
+                <p className="font-medium px-4 py-1 bg-chat-bg rounded-2xl">
+                    Select a chat to start messaging
+                </p>
+            </div>
         </div>
     );
 }
