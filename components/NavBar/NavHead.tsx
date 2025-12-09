30c37aed51d11d96cc343809a748601fe36a9369
lvd536 <nbikiti@gmail.com>
2025-12-09 19:03:00 +0400

    - Add logo in nav panel

--- components/NavBar/NavHead.tsx
+++ components/NavBar/NavHead.tsx
@@ -1,5 +1,3 @@
-import { User } from "firebase/auth";
-import Link from "next/link";
 import { Dispatch, SetStateAction, useEffect } from "react";
 
 interface IProps {
@@ -20,9 +18,54 @@ export default function NavHead({
     }, [searchValue.length, setOnSearch]);
 
     return (
-        <div className="flex gap-2 justify-center items-center mt-3 px-4">
+        <div className="flex flex-col gap-2 justify-center items-center mt-3 px-4">
+            <div className="flex gap-2 items-center">
+                <svg
+                    width="40"
+                    height="40"
+                    viewBox="0 0 512 512"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <defs>
+                        <linearGradient
+                            id="purple_gradient"
+                            x1="0"
+                            y1="0"
+                            x2="512"
+                            y2="512"
+                            gradientUnits="userSpaceOnUse"
+                        >
+                            <stop stop-color="#8E2DE2" />
+                            <stop offset="1" stop-color="#4A00E0" />
+                        </linearGradient>
+                    </defs>
+
+                    <circle
+                        cx="256"
+                        cy="256"
+                        r="230"
+                        fill="url(#purple_gradient)"
+                    />
+
+                    <path
+                        d="M256 120C176.5 120 112 177.3 112 248C112 288.6 134.8 324.7 170.7 348L158 392L208.6 369.8C223.4 374.5 239.3 377.2 256 376C335.5 376 400 318.7 400 248C400 177.3 335.5 120 256 120Z"
+                        fill="white"
+                        fill-opacity="0.2"
+                    />
+
+                    <path
+                        d="M266 160L216 256H256L236 336L306 226H256L266 160Z"
+                        fill="white"
+                        stroke="white"
+                        stroke-width="10"
+                        stroke-linejoin="round"
+                    />
+                </svg>
+                <p className="font-bold">Chattify</p>
+            </div>
             <form
-                className="flex relative"
+                className="flex relative w-full"
                 action=""
                 onSubmit={(e) => {
                     e.preventDefault();
@@ -33,7 +76,7 @@ export default function NavHead({
                 <input
                     type="search"
                     placeholder="Search..."
-                    className="block h-8 p-2 lg:p-4 min-w-20 items-center rounded-lg bg-search-bar"
+                    className="block h-8 p-2 lg:p-4 w-full items-center rounded-lg bg-search-bar"
                     value={searchValue}
                     onChange={(e) => setSearchValue(e.target.value)}
                 />
