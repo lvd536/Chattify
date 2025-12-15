b5e9f4eed68c66724b33219c4d38f0b2a89850c1
lvd536 <nbikiti@gmail.com>
2025-12-16 01:04:33 +0400

    - Fix create group btn issue

--- components/NavBar/CreateGroupBtn.tsx
+++ components/NavBar/CreateGroupBtn.tsx
@@ -7,7 +7,7 @@ interface IProps {
 export default function CreateGroupBtn({ uid }: IProps) {
     return (
         <Link
-            href={`home/create-group/${uid}`}
+            href={`/home/create-group/${uid}`}
             className="absolute right-1 bottom-3 px-4 py-2 bg-auth-input hover:bg-auth-input-hover transition-bg duration-300 rounded-4xl"
         >
             + Create group
