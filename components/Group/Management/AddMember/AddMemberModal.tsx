71e8ba4a0d63b71c926e5f6b205a8ef0b8c69db0
lvd536 <nbikiti@gmail.com>
2025-12-16 21:40:16 +0400

    - Slit add member model to single components

--- components/Group/Management/AddMember/AddMemberModal.tsx
+++ components/Group/Management/AddMember/AddMemberModal.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import FoundList from "../../Creation/FoundList";
 import MemberSearch from "../../Creation/MemberSearch";
 import { IHasMembers } from "@/types/IHasMembers";
-import Avatar from "../../Creation/Avatar";
 import { addMembersToGroup } from "@/utils/group";
+import MemberModalActions from "./MemberModalActions";
+import MemberModalList from "./MemberModalList";
 
 interface IProps {
     uid: string;
@@ -37,47 +38,14 @@ export default function AddMemberModal({ uid, groupId, setIsActive }: IProps) {
     };
     return (
         <div className="fixed top-1/24 right-1/24 sm:top-1/4 sm:right-1/4 p-2 bg-black/40 rounded-lg w-11/12 sm:w-1/2 h-11/12 sm:h-1/2">
-            <div className="flex items-center justify-between">
-                <button
-                    className="p-2 rounded-lg bg-auth-input hover:bg-auth-input-hover transition-bg duration-300"
-                    onClick={handleAddMembers}
-                >
-                    Add members
-                </button>
-                <button
-                    className="p-2 rounded-lg bg-auth-input-bg hover:bg-auth-input-bg/90 transition-bg duration-300"
-                    onClick={() => setIsActive(false)}
-                >
-                    Cancel
-                </button>
-            </div>
-            <ul className="flex gap-2 my-2">
-                {formData.members.length > 0 ? (
-                    formData.members.map((member) => (
-                        <li
-                            key={member.uid}
-                            className="flex items-center justify-between gap-2 p-2 bg-audio-player w-fit rounded-full"
-                        >
-                            <div className="flex items-center gap-2">
-                                <Avatar
-                                    src=""
-                                    alt="User avatar"
-                                    name={member.displayName || member.username}
-                                />
-                                <p className="text-sm">{member.displayName}</p>
-                            </div>
-                            <button
-                                className="text-xs ml-2 hover:text-text/50 transition-text duration-300"
-                                onClick={() => handleMemberDelete(member.uid)}
-                            >
-                                x
-                            </button>
-                        </li>
-                    ))
-                ) : (
-                    <li className="text-sm text-text/50">No members</li>
-                )}
-            </ul>
+            <MemberModalActions
+                handleAddMembers={handleAddMembers}
+                setIsActive={() => setIsActive(false)}
+            />
+            <MemberModalList
+                handleMemberDelete={handleMemberDelete}
+                members={formData.members}
+            />
             <MemberSearch
                 searchValue={searchValue}
                 setOnSearch={setOnSearch}
